78e36689b61dccfb1602e8d082c3a9582bc035c3
Marie-AnneS <marie-anne.seim@gmail.com>
2019-05-28 15:51:45 +0000

    adding OO javascrip has a exercise

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-// Arrays to keep track of each task's state
+function newTask(title, description){
+  const task = {
+    title,
+    description,
+    complete: false,
+    logState: function() {
+      console.log(`${task.title} has${task.complete ? " " : " not "}been completed`)
+    },
+    markCompleted: function() {
+      task.complete = true;
+    }
+    
+  }
+  return task;
+}
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState(); 
+task1.markCompleted();
+task1.logState();
+
+/* function
+
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`)
+}
+
+function completeTask(task) {
+  task.complete = true;
+}
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); 
+completeTask(task1);
+logTaskState(task1); 
+
+console.log(tasks); */
+
+/* // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
+  taskDescriptions.push(description);
   taskComplete.push(false);
 }
 
@@ -28,4 +72,4 @@ newTask("Do Laundry"); // task 1
 
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+logTaskState(0); // Clean Cat Litter has been completed */
